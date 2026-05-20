ebbf69acdefdc5fad9748d8ae4c31c25a8c8d56e
Nanatokka <app.lab.nanato@gmail.com>
2026-05-20 17:46:57 +0900

    add ios app icon

--- android/app/src/main/kotlin/com/example/new_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/new_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.new_app
+package jp.nanatokka.multiplatformdata
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ android/app/src/main/kotlin/jp/nanatokka/multiplatformdata/MainActivity.kt
@@ -0,0 +1,5 @@
+package jp.nanatokka.multiplatformdata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
